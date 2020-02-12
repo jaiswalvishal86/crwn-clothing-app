18f687753abc6423abe87a64cc6ae9846c65e768
Vishal Jaiswal <jaiswalvishal86@gmail.com>
2020-02-12 20:08:29 +0530

    moved our directory and shop data into its own reducer, and created respective selector and updated corresponding components with new redux flow for directory/shop

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
-import { addItemToCart } from '../../redux/cart/cart.utils';
+// import { addItemToCart } from '../../redux/cart/cart.utils';
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
 	const { name, imageUrl, quantity, price } = cartItem;
